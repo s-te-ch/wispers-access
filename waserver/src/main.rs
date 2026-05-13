fd170de4d581f601318198b1d5549dd1b05030f2
Matthias Scheidegger <mscheidegger@gmail.com>
2026-05-13 23:57:51 +0200

    Add plan for each command, in comments

--- waserver/src/main.rs
+++ waserver/src/main.rs
@@ -70,30 +70,58 @@ fn run() -> Result<()> {
 
 fn init(api_key: &str, share: &str) -> Result<()> {
     println!("init({}, {});", api_key, share);
+    // What this needs to do:
+    // - Create a connectivity group using the Rest API (name TBD, something
+    //   like "Wispers Access - $name"?)
+    // - Store this as one file in the config dir (key and cgID)
+    // - Get a registration code for node 1, name "waserver"
+    // - Create a Node, with storage implementation using another file in the
+    //   config dir
+    // - Register the node (this writes automatically)
     Ok(())
 }
 
 fn serve(share: &str, local_port: &u16) -> Result<()> {
     println!("serve({}, {});", share, local_port);
+    // What this needs to do:
+    // - Read the config of the given share (api key and cgID), which we'll use
+    //   for invites
+    // - Init the node (must be registered or activated)
+    // - Serve in the foreground (this part is mostly what wconnect did, except
+    //   we need to handle the requests, so we can inject or elide headers)
     Ok(())
 }
 
 fn start(share: &str, local_port: &u16) -> Result<()> {
     println!("start({}, {});", share, local_port);
+    // What this needs to do:
+    // - Daemonise (see wconnect)
+    // - Serve
     Ok(())
 }
 
 fn stop(share: &str) -> Result<()> {
     println!("stop({});", share);
+    // What this needs to do:
+    // - Find the server (UDS on *NIX, port on Windows)
+    // - Tell it to shut down
+    // (this should work with both foreground and daemon servers)
     Ok(())
 }
 
 fn status() -> Result<()> {
     println!("status();");
+    // What this needs to do:
+    // - List all shares (using the file system)
+    // - For each, check if there's a server running
+    // - If so, query the server for info/stats to display
+    // - Print it all nicely
     Ok(())
 }
 
 fn logs(follow: &bool, share: &str) -> Result<()> {
     println!("logs({}, {});", follow, share);
+    // - Find the latest logs of the given share (platform dependent)
+    // - Print them, or if -f is given, tail them
     Ok(())
 }
