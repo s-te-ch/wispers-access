7ae63f9b66ba1b6c9901963749d5aa7321afc008
Matthias Scheidegger <mscheidegger@gmail.com>
2026-05-18 15:10:47 +0200

    Add IPC server-side implementation

--- waserver/src/serving.rs
+++ waserver/src/serving.rs
@@ -1,9 +1,11 @@
 //! Serving logic - handles hub connection and incoming P2P connections.
 
+use crate::ipc;
 use crate::storage;
 use crate::wcbe;
 use anyhow::Result;
 use wispers_connect as wc;
+use tokio::io::{AsyncWriteExt};
 
 pub async fn serve(share: &str, _port: &u16) -> Result<()> {
     let store = storage::ShareStateStore::new(share)?;
@@ -16,9 +18,24 @@ pub async fn serve(share: &str, _port: &u16) -> Result<()> {
     if !node.is_registered() {
         anyhow::bail!("Wispers Connect node is not registered");
     }
-    // TODO:
-    // - Open IPC (UDS or local port)
-    // - Serving loop
-    
-    Ok(())
+    let ipc_server = ipc::Server::bind(share).await?;
+
+    loop {
+        tokio::select! {
+            // New IPC connection.
+            result = ipc_server.accept() => {
+                match result {
+                    Ok(stream) => {
+                        let (_, mut writer) = stream.into_split();
+                        if let Err(e) = writer.write_all(b"hello\n").await {
+                            eprintln!("Failed to write response: {}", e);
+                        }
+                    }
+                    Err(e) => {
+                        eprintln!("Failed to accept daemon connection: {}", e);
+                    }
+                }
+            }
+        }
+    }
 }
