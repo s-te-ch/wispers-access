54b951c828f5cf4186686949a68beae8446a3579
Matthias Scheidegger <mscheidegger@gmail.com>
2026-05-23 13:29:31 +0200

    Add multi-node connection pool

--- waclient/src/main.rs
+++ waclient/src/main.rs
@@ -3,6 +3,9 @@ use clap::{Parser, Subcommand};
 use std::collections::HashMap;
 use std::fs;
 use std::path::PathBuf;
+use std::sync::Arc;
+use std::time::Instant;
+use tokio::sync::{Mutex, OnceCell};
 use wispers_connect as wc;
 
 #[derive(Parser)]
@@ -30,7 +33,9 @@ enum Command {
 fn main() -> Result<()> {
     // Restrict default file mode to user-only. Safe to do as the first thing.
     #[cfg(unix)]
-    unsafe { libc::umask(0o077); }
+    unsafe {
+        libc::umask(0o077);
+    }
     let cli = Cli::parse();
     tokio::runtime::Builder::new_multi_thread()
         .enable_all()
@@ -46,7 +51,7 @@ async fn async_main(command: Command) -> Result<()> {
     }
 }
 
-async fn join(share : &str, invite_code: &str) -> Result<()> {
+async fn join(share: &str, invite_code: &str) -> Result<()> {
     let Some((registration_token, activation_code)) = invite_code.split_once('/') else {
         anyhow::bail!("invalid invite code");
     };
@@ -71,11 +76,12 @@ async fn join(share : &str, invite_code: &str) -> Result<()> {
 
 async fn serve(_port: u16) -> Result<()> {
     let shares = list_shares()?;
-    let mut nodes = HashMap::new();
+    let mut nodes = Vec::new();
     for share in &shares {
         let node = load_node(share).await?;
-        nodes.insert(share.clone(), node);
+        nodes.push((share.clone(), node));
     }
+    let _sf = StreamFactory::new(nodes);
 
     println!("Found shares:");
     for share in &shares {
@@ -117,10 +123,72 @@ fn list_shares() -> Result<Vec<String>> {
             .collect()
     };
     Ok(shares)
-
 }
 
 fn get_storage_dir() -> Result<PathBuf> {
     let config_dir = dirs::config_dir().context("could not determine config directory")?;
     Ok(config_dir.join("waclient"))
+}
+
+struct StreamFactory {
+    nodes: HashMap<String, wc::Node>,
+    pool: Mutex<HashMap<String, PoolEntry>>,
+}
+
+impl StreamFactory {
+    fn new(nodes: Vec<(String, wc::Node)>) -> Self {
+        Self {
+            nodes: nodes.into_iter().collect(),
+            pool: Mutex::new(HashMap::new()),
+        }
+    }
+
+    async fn open_stream(&self, share: &str) -> Result<wc::QuicStream> {
+        let Some(node) = self.nodes.get(share) else {
+            anyhow::bail!("Unknown share {}", share);
+        };
+        // Open a stream with a single retry. This covers the case when the
+        // connection has died and needed reestablishing.
+        match self.try_open_stream(share, &node).await {
+            Ok(s) => Ok(s),
+            Err(_) => self.try_open_stream(share, &node).await,
+        }
+    }
+
+    async fn try_open_stream(&self, share: &str, node: &wc::Node) -> Result<wc::QuicStream> {
+        // Get the cell under lock and update last_used.
+        let cell = {
+            let mut pool = self.pool.lock().await;
+            let pool_entry = pool.entry(share.to_owned()).or_insert_with(|| PoolEntry {
+                cell: Arc::new(OnceCell::new()),
+                last_used: Instant::now(),
+            });
+            pool_entry.last_used = Instant::now();
+            pool_entry.cell.clone()
+        };
+        // Get or establish the connection.
+        let conn = cell
+            .get_or_try_init(|| async { node.connect_quic(1).await.map(Arc::new) })
+            .await?
+            .clone();
+        // Open a stream. If this fails, the underlying connection has
+        // broken and we should remove it from the pool.
+        match conn.open_stream().await {
+            Ok(stream) => Ok(stream),
+            Err(e) => {
+                let mut pool = self.pool.lock().await;
+                if let Some(entry) = pool.get(share)
+                    && Arc::ptr_eq(&entry.cell, &cell)
+                {
+                    pool.remove(share);
+                }
+                Err(e.into())
+            }
+        }
+    }
+}
+
+struct PoolEntry {
+    cell: Arc<OnceCell<Arc<wc::QuicConnection>>>,
+    last_used: Instant,
 }
