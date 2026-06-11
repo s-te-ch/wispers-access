b37d2b2b58800718ae5257fa651339130e30f223
Matthias Scheidegger <mscheidegger@gmail.com>
2026-06-11 18:39:08 +0200

    Apparently version 1 was already used...

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         applicationId = "dev.wispers.access.android"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
