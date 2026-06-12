4bbce89f3b9d21eca04e55a5914e13852c5cb961
Matthias Scheidegger <mscheidegger@gmail.com>
2026-06-12 13:56:15 +0200

    Bump Android app to 3 (0.1.1)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "dev.wispers.access.android"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.0"
+        versionCode = 3
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
