346a259bdd015399b1fa8dada69a0d8c1d295e88
Matthias Scheidegger <mscheidegger@gmail.com>
2026-07-24 14:53:18 +0200

    Bump Android to v0.2.2 (6)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "dev.wispers.access.android"
         minSdk = 28
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.2.1"
+        versionCode = 6
+        versionName = "0.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
